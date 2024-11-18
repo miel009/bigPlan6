1a92e4da4ef4568bf4c5099da8eb8cf50f96ada8
NineTravel <melanyascencio908@gmail.com>
2024-11-17 22:58:40 -0300

    feat: Implement new activity for the home screen

--- app/src/main/java/com/example/bigplan/MainActivity1.java
+++ app/src/main/java/com/example/bigplan/MainActivity1.java
@@ -1,6 +1,9 @@
 package com.example.bigplan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity1 extends AppCompatActivity {
@@ -9,5 +12,10 @@ public class MainActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/bigplan/ScrollingActivity.java
@@ -0,0 +1,28 @@
+package com.example.bigplan;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.bigplan.databinding.ActivityScrollingBinding;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private ActivityScrollingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
